fe6fe20af95409a87ee78210c766bc55d157a35c
Emmanuel Cajas <emmanuelcajasballesteros@gmail.com>
2023-02-24 06:15:39 +0100

    tasca5N1ejercicio2 casi terminado

--- src/tasca5/N1/ejercicio2/Main.java
+++ src/tasca5/N1/ejercicio2/Main.java
@@ -5,18 +5,64 @@ import java.util.Arrays;
 
 public class Main {
 
+	public static void listarDirectorio(File f, String separador){
+		File[] ficheros = f.listFiles();
+				
+		for (int x=0;x<ficheros.length;x++){	
+			
+			String directorioFile;
+			
+			if(ficheros[x].isDirectory()) {
+				directorioFile = "D";
+			}else {
+				directorioFile = "F";
+			}
+			
+			System.out.println(separador + ficheros[x].getName() + "   "+directorioFile + "   "+ficheros[x].lastModified());
+			
+			if (ficheros[x].isDirectory()){
+				String nuevo_separador;
+				nuevo_separador = separador + " ";
+				listarDirectorio(ficheros[x],nuevo_separador);
+			}
+		}	
+	}
+	
+	
 	public static void main(String[] args) {
 		
+		String sDirectorio = "c:/Users/rodri/Desktop/eclipse/Sprint_1";
+		File directorio = new File(sDirectorio);
 		
-		File directorio = new File("c:/Users/rodri/Desktop");
-		File[] listaDirectorios = directorio.listFiles();
-		
-		Arrays.sort(listaDirectorios);
-		
-		
-			for (File listado : listaDirectorios) {
-				System.out.println(listado);
-			}
-	
+		listarDirectorio(directorio,"");
+
 	}
+
+//	public static void listar(File directorio, String separador) {
+//
+//		File[] ficheros = directorio.listFiles();
+//		Arrays.sort(ficheros);
+//		for (int i = 0; i < ficheros.length; i++) {
+//
+//			if (ficheros[i].isDirectory()) {
+//				
+//				System.out.println(ficheros[i].getName() + "  D");
+//				String nuevoSeparador;
+//				nuevoSeparador = separador + " ";
+//				
+//				listar(ficheros[i],nuevoSeparador);
+//			}else {
+//				System.out.println(ficheros[i].getName() + "  F");
+//			}
+//		}
+//
+//	}
+//
+//	public static void main(String[] args) {
+//
+//		File directorio = new File("c:/Users/rodri/Desktop/eclipse/Sprint_1");
+//		String separador ="";
+//		listar(directorio,separador);
+//
+//	}
 }
